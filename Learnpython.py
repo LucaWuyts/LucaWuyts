b47059997a0dc3549eb6d9d79812ee96243f2917
Luca Wuyts <luca.wuyts04@gmail.com>
2022-04-15 18:31:01 +0200

    15-04-2022 18:30

---
+++ Learnpython.py
@@ -0,0 +1,10 @@
+
+#Les 1
+#print ("Hello World!!!!")
+#print ('I am Iron Man')
+#print ("No, I'm Tony Stark")
+#print ("No, I am Poppy")
+#We are practicing Python ... IT'S AWESOME!
+#print ("I am Iron Man.\n" + "No, I am Tony Stark.\n" + "No, I am Poppy."  )
+#print("I am Poppy\n" * 100)
+
