739cc2e8680405536b16cc8c5860f2b8d3aeaed5
Luca Wuyts <luca.wuyts04@gmail.com>
2022-04-15 18:46:36 +0200

    Les 2

--- Learnpython.py
+++ Learnpython.py
@@ -8,3 +8,13 @@
 #print ("I am Iron Man.\n" + "No, I am Tony Stark.\n" + "No, I am Poppy."  )
 #print("I am Poppy\n" * 100)
 
+#Let's build a robot Barista!!
+
+print("Hello, welcome to NetworkChuck Coffee!!!")
+
+name = input("What is your name?")
+
+print(name)
+
+#print("Hello ..... ,thank you so much for coming in today!")
+
