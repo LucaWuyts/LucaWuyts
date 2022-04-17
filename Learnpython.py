8b4aee563889e7299fedf3a39d7b3dd8000fc205
Luca Wuyts <luca.wuyts04@gmail.com>
2022-04-17 11:23:28 +0200

    Robot Barista

--- Learnpython.py
+++ Learnpython.py
@@ -12,9 +12,14 @@
 
 print("Hello, welcome to NetworkChuck Coffee!!!")
 
-name = input("What is your name?")
+name = input("What is your name?\n")
 
-print(name)
+print("Hello " + name + " ,thank you so much for coming in today!\n\n\n")
 
-#print("Hello ..... ,thank you so much for coming in today!")
+menu = "Black Coffee, Espresso, Latte, Cappuccino"
 
+print(name + " ,what would you like from our menu today? Here is what we are serving. \n" + menu)
+
+order = input()
+
+print("Sounds good " + name + " ,we'll have that " + order + " ready for you in a moment.")
